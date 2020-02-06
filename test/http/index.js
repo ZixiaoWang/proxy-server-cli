7041af23fe99ae80c095161335a31b4ac72170b8
ZixiaoWang <www.wangzixiao@gmail.com>
2020-02-06 15:22:15 +0800

    fixed a bug in test case

--- test/http/index.js
+++ test/http/index.js
@@ -5,14 +5,18 @@ const express = require('express');
 const axios = require('axios');
 const case_array = require('./cases');
 const faker = require('faker');
+const proxy = require('../../dist/server/index.js');
 
 const case_map = new Map();
 const app = express();
 
 const cases = case_array.map(item => {
+    if (item.method === 'get') {
+        return item;
+    }
+    
     const id = faker.random.uuid();
     item.request.id = id;
-
     return {
         id, 
         ...item
@@ -26,12 +30,24 @@ cases.forEach(item => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use((req, res) => {
+    if (req.method === 'get') {
+        res.status(200);
+    }
+
     const id = req.body.id;
     const item = case_map.get(id);
     res.send(item.response);
 })
 
-app.listen(9000, async () => {
+const test_server = app.listen(9000, async () => {
+    const options = {
+        ws: false,
+        changeOrigin: true,
+        port: 3000,
+        target: 'http://localhost:9000'
+    }
+    const proxy_server = await proxy.start_server(options);
+
     const promise_list = cases.map(async (item) => {
         try {
             const proxy_url = 'http://localhost:3000' + item.path;
@@ -56,5 +72,6 @@ app.listen(9000, async () => {
     const results = await Promise.all(promise_list);
     console.log(results);
 
-    process.exit(0);
+    proxy_server.close();
+    test_server.close();
 });
